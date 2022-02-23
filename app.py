cda330435f7f65098515903c81d662e80c73c648
williamle92 <william1992le@gmail.com>
2022-02-23 12:41:58 -0800

    created temporary view

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
 from config import Config
+from db import db
 
 # create flask app
 app = Flask(__name__)
@@ -9,5 +10,18 @@ app = Flask(__name__)
 # Get configs from Config object
 app.config.from_object(Config)
 
-
+# instantiating migrate object
 migrate = Migrate(app,db)
+
+
+# instantiate db
+db.init_app(app)
+
+# instantiate API object
+api = Api(app)
+
+
+# temporary view
+@app.route('/')
+def home():
+    return "Hello World!"
