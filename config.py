9cc566057b3633d9a01ceeef6e6d59d1013a67e4
williamle92 <william1992le@gmail.com>
2022-02-23 12:38:05 -0800

    added .env and configs to api

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+# Getting config from env
+class Config():
+    FLASK_APP = os.environ.get('FLASK_APP')
+    FLASK_ENV = os.environ.get('FLASK_ENV')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = os.getenv("SECRET_KEY", "secret-key")
