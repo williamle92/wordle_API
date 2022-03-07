36ec81dd9965f6c45579bcb907f5a25e3e9f4e9a
williamle92 <william1992le@gmail.com>
2022-03-07 02:01:23 -0800

    created delete route for game and update route

--- resources/game.py
+++ resources/game.py
@@ -11,7 +11,7 @@ class GameResource(Resource):
     def get(self, id):
         current_user = get_jwt_identity()
         game = Game.query.filter_by(creator_id=current_user,id=id).first()
-        print(game)
+
         if game:
             return game.json(), 200
         return {"Message": "The game ID associated with your account could not be found. Please try Again"}, 404
@@ -21,8 +21,6 @@ class GameResource(Resource):
     def post(self):
         current_user = get_jwt_identity()
         user = User.query.filter_by(id=current_user).first()
-        print(user.json())
-        print(user.guesses)
         
         if user:
             game = Game(creator_id =user.id)
@@ -31,23 +29,43 @@ class GameResource(Resource):
             try:
                 game.save_to_db()
             except:
-                {"Message": "An error occured while processing your request"}, 500
+                return {"Message": "An error occured while processing your request"}, 500
             return game.json(), 201
-        return {"Message": "please include a username with your request"}
+
 
     
     @jwt_required()
     def put(self,id):
-        data = GameResource
-        game = Game.find_by_id(id)
+        current_user = get_jwt_identity()
+        game = Game.query.filter_by(creator_id=current_user, id=id).first()
+        add = request.get_json().get('add', '')
+        remove = request.get_json().get('remove', "")
+        user_add = User.query.filter_by(username=add).first()
+        user_remove = User.query.filter_by(username=remove).first()
 
+        if not add and not remove:
+            return {"Message": "Error! can only add or remove user one at a time. Please use keys 'add': username to add, or 'remove': username to remove a user"}
         if game is None:
             game = Game()
-        else:
-            game.users = data["users"]
-            game.status = data['status']
+        if add and user_add:
+            game.users.append(user_add)
+        if remove and user_remove:
+            game.users.remove(user_remove)
         game.save_to_db
-        return game.json()
+        return game.json(), 200
+
+
+    @jwt_required()
+    def delete(self, id):
+        current_user = get_jwt_identity()
+        game = Game.query.filter_by(creator_id = current_user, id=id).first()
+        if game:
+            try:
+                game.delete_from_db()
+            except:
+                return {"Message": "You do not have the authorization to delete this game"}, 403
+            return {"Message": "The game has successfully been deleted"}, 200
+      
 
 
 class Games(Resource):
--- schema.py
+++
@@ -1,8 +0,0 @@
-from flask_marshmallow import Marshmallow
-
-ma = Marshmallow()
-
-
-
-class UserSchema(ma.Schema):
-    fields = ('username', )
